44ca2bbe2132201e79bd403939fa79ea5f65c223
erinkelsey <erinszilagyi@gmail.com>
2022-01-05 16:20:13 -0500

    Adding custom form input and button with files

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,7 +4,6 @@ import "./form-input.styles.scss";
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className='group'>
-    <input className='form-input' onChange={handleChange} {...otherProps} />
     {label ? (
       <label
         className={`${otherProps.value.length ? "shrink" : "form-input-label"}`}
@@ -12,6 +11,7 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
         {label}
       </label>
     ) : null}
+    <input className='form-input' onChange={handleChange} {...otherProps} />
   </div>
 );
 
